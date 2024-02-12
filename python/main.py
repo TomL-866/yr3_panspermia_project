cb81d2b6fee9debd2fd579707234e736deb68524
SprintingSand <tomlocks1@gmail.com>
2024-02-12 17:34:25 +0000

    so fast, no csv for now

--- python/main.py
+++ python/main.py
@@ -2,7 +2,7 @@
 
 import numpy as np
 
-from python.functions import plot_imf_histogram, save_imf_values
+from python.functions import plot_imf_histogram
 from python.equations import quantile_func
 
 
@@ -11,10 +11,8 @@ def main() -> None:
 
     runs: int = 1_000_000
 
-    u = np.random.uniform(0, 1, runs)
-    stellar_mass_arr = np.vectorize(quantile_func)(u)  # SI units
+    u: np.ndarray = np.random.uniform(0, 1, runs)
+    stellar_mass_arr: np.ndarray = np.vectorize(quantile_func)(u)  # SI units
 
-    print("Saving IMF values to CSV...")
-    save_imf_values(u, stellar_mass_arr)
     print("Plotting IMF histogram...")
     plot_imf_histogram(stellar_mass_arr)
