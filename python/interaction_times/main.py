349e40ef95d98f64c6597e2f67f73834a3d02137
SprintingSand <146181634+SprintingSand@users.noreply.github.com>
2024-02-27 15:21:17 +0000

    double checking collision time and saving

--- python/interaction_times/main.py
+++ python/interaction_times/main.py
@@ -1,4 +1,6 @@
+import numpy as np
 from interaction_times.equations import t_coll
+from helpers import get_base_dir
 
 
 def main() -> None:
@@ -8,7 +10,7 @@ def main() -> None:
     n_o: float = 0.2 * 1 / (au_to_m**3)  # SI units
 
     # https://doi.org/10.1038/nature25020 gives the velocity of 'Oumuamua-like object as 26 km/s
-    # see also 	https://doi.org/10.1051/0004-6361/202141283
+    # see also https://doi.org/10.1051/0004-6361/202141283
     # This is speed relative to the Sun.
     v_o: float = 26e3  # SI units
 
@@ -16,6 +18,11 @@ def main() -> None:
         365 + 0.25 - 0.01 + 0.0025 - 0.00025
     )  # 365.2425 days for Gregorian calendar
 
+    collision_time = t_coll(n_o, v_o)
+
     print(
-        f"Time for collision: {t_coll(n_o, v_o) / (days_in_a_year * 24 * 60 * 60 * 1e6)} Myr"
+        f"Time for collision: {collision_time / (days_in_a_year * 24 * 60 * 60 * 1e6)} Myr"
     )
+
+    # Write the collision time to file
+    np.save(f"{get_base_dir()}/output/values/collision_time.npy", collision_time)
